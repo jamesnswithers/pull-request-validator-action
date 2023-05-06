48c22e501f6dad93113623f396e59e0059eeb0e2
James Withers <jamesnswithers@gmail.com>
2023-05-06 18:05:35 +0100

    remove unused vars and function removed await

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,11 @@ import { StatusStates } from "./statusStates";
 import { isTitleValid } from "./validateTitle";
 
 const eventTypes = ['pull_request'];
-const actionsToCheckTitle = ['opened', 'reopened', 'edited', 'synchronize'];
 
 async function run() {
   const github_token = core.getInput('github-token', { required: true });
   const gitHubClient = new github.GitHub(github_token);
   const config = await getConfig(gitHubClient);
-  const pullRequestSha = github!.context!.payload!.pull_request!.head!.sha;
   const context = github!.context;
   const payload = context!.payload;
   const action = payload!.action || '';
@@ -24,11 +22,9 @@ async function run() {
   }
   core.info('The action is: ' + action);
 
-  const shouldCheckTitle = _.includes(actionsToCheckTitle, action);
-
   if (_.hasIn(config , 'checks.title-validator')) {
     const pullRequestTitle = payload!.pull_request!.title;
-    const titleCheckState = await isTitleValid(pullRequestTitle, _.get(config, 'checks.title-validator.matches'));
+    const titleCheckState = isTitleValid(pullRequestTitle, _.get(config, 'checks.title-validator.matches'));
     if (!titleCheckState) {
       core.setFailed("Pull Request Title Validation Failed")
     }
