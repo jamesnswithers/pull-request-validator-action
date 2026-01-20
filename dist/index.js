eff7a3a0fb21f3f443a59a25a8d68f9d73fa5851
James Withers <jamesnswithers@gmail.com>
2026-01-20 23:05:44 +0000

    more tests

--- dist/index.js
+++ dist/index.js
@@ -184,7 +184,12 @@ function run() {
                 _.hasIn(config, "checks.title-validator.failure-message")) {
                 //core.error(_.get(config, "checks.title-validator.failure-message"));
                 core.summary.addHeading('Pull Request Title Validation Failed', '2');
+                core.summary.addEOL();
                 const failureMessage = _.get(config, "checks.title-validator.failure-message");
+                failureMessage.split('\n').forEach(line => {
+                    core.info(line);
+                    core.summary.addRaw(line, true);
+                });
                 failureMessage.split('\r\n').forEach(line => {
                     core.info(line);
                     core.summary.addRaw(line, true);
