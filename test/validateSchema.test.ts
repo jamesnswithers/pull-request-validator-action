a53d7e6f7aa79aefc9455ef8aebb755e28072373
James Withers <jamesnswithers@gmail.com>
2026-02-07 08:11:59 +0000

    update and simplify tests

--- test/validateSchema.test.ts
+++ test/validateSchema.test.ts
@@ -1,7 +1,8 @@
-import { schema } from '../lib/validateSchema';
-const { validSchemas, invalidSchemas } = require('../test/resources/schema/object/validateSchema');
-import * as fs from 'fs';
-import * as yaml from 'js-yaml';
+import { ValidationError } from 'joi';
+import { validateConfig } from '../src/validateSchema';
+import { validSchemas, invalidSchemas } from './resources/schema/object/validateSchema';
+import fs from 'fs';
+import yaml from 'js-yaml';
 
 const VALID_TEST_RESOURCES = 'test/resources/schema/yaml/valid';
 const INVALID_TEST_RESOURCES = 'test/resources/schema/yaml/invalid';
@@ -9,28 +10,25 @@ const INVALID_TEST_RESOURCES = 'test/resources/schema/yaml/invalid';
 describe('schema', () => {
   describe('valid', () => {
     validSchemas.forEach(([schemaName, schemaMatch]) => {
-      const file = VALID_TEST_RESOURCES + '/' + schemaName + '.yaml';
+      const file = `${VALID_TEST_RESOURCES}/${schemaName}.yaml`;
       test(`${file} is valid`, () => {
-        const { error, value } = schema.validate(yaml.load(fs.readFileSync(file, 'utf8')), {
-          abortEarly: false
-        });
-        expect(error).toBeUndefined();
-        expect(JSON.stringify(value)).toMatch(JSON.stringify(schemaMatch));
+        const validationResult = validateConfig(yaml.load(fs.readFileSync(file, 'utf8')));
+        expect(validationResult.error).toBeUndefined();
+        expect(JSON.stringify(validationResult)).toMatch(JSON.stringify(schemaMatch));
       });
     });
   });
-})
+});
 
 describe('schema', () => {
   describe('invalid', () => {
     invalidSchemas.forEach(([schemaName, schemaError]) => {
-      const file = INVALID_TEST_RESOURCES + '/' + schemaName + '.yaml';
+      const file = `${INVALID_TEST_RESOURCES}/${schemaName}.yaml`;
       test(`${file} is invalid`, () => {
-        const { error, value } = schema.validate(yaml.load(fs.readFileSync(file, 'utf8')), {
-          abortEarly: false
-        });
-        expect(error && error.toString()).toMatch(schemaError);
+        const validationResult = validateConfig(yaml.load(fs.readFileSync(file, 'utf8')));
+        expect(validationResult).toBeInstanceOf(ValidationError);
+        expect(validationResult && validationResult.toString()).toMatch(schemaError);
       });
     });
   });
-})
+});
