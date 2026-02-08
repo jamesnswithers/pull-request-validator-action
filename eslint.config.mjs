489004483ce8417b7a47be629a5857239df079bd
James Withers <jamesnswithers@gmail.com>
2026-02-08 10:07:46 +0000

    prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import {defineConfig, globalIgnores} from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   tseslint.configs.stylistic,
-  globalIgnores(["lib/", "dist/"]),
+  globalIgnores(['lib/', 'dist/'])
 );
